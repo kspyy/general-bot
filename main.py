ab0dfd16d99b13d95e14dc0bdaf7fed68b13a573
kspyy <krispey@gmail.com>
2024-04-12 21:43:09 -0700

    test change behavior

--- main.py
+++ main.py
@@ -83,7 +83,9 @@ async def on_message(message):
 
     if message.content == 'git':
         await message.channel.send('working!')
-    
+        
+    if message.content == 'commit':
+        await message.channel.send('commit works!') 
         
     if message.content == 'thicc me':
         await message.channel.send('https://e7.pngegg.com/pngimages/194/801/png-clipart-shaquille-o-neal-the-general-vehicle-insurance-car-car.png')
