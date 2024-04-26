b4a61cb118261b9239c4e4d2836c8238728bac97
kspyy <krispey@gmail.com>
2024-04-26 00:08:39 -0400

    Update main.py
    set bnb response to string

--- upload/main.py
+++ upload/main.py
@@ -125,6 +125,6 @@ async def on_message(message):
         await message.channel.send('https://e7.pngegg.com/pngimages/194/801/png-clipart-shaquille-o-neal-the-general-vehicle-insurance-car-car.png')
 
     if message.content == 'airbnb':
-        await message.channel.send(getListingData())
+        await message.channel.send(str(getListingData()))
 
 client.run(token)
