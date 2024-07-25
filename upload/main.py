b9ac2f1e8dcc30c4198a457c3cccee9952666909
kspyy <krispey@gmail.com>
2024-07-24 20:39:40 -0700

    Update main.py

--- upload/main.py
+++ upload/main.py
@@ -88,6 +88,7 @@ async def on_message(message):
         await message.channel.send('https://e7.pngegg.com/pngimages/194/801/png-clipart-shaquille-o-neal-the-general-vehicle-insurance-car-car.png')
 
     if message.content == 'bir':
-        await message.channel.send(birthday_checker)
+        b = birthday_checker()
+        await message.channel.send(b)
 
 client.run(token)
